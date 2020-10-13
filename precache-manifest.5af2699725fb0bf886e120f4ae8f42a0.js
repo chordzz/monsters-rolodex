398933a5dba90d4cd76def6481c96103dce0f5f9
Olaife Olawore <olaifeolawore@gmail.com>
2020-10-13 13:36:52 +0100

    Updates

---
+++ precache-manifest.5af2699725fb0bf886e120f4ae8f42a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584efa5c58114d8a5f42aeab4c83e2b3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4a5fe44fa70b76096672",
+    "url": "/monsters-rolodex/static/css/main.e4c8d976.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a5fe44fa70b76096672",
+    "url": "/monsters-rolodex/static/js/main.eb49a440.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
